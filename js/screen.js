fab03668c2a4c8ba2973c2e9f7775c3e9beba7c7
devGnode <looterofflux@gmail.com>
2017-04-20 23:48:01 +0200

    Update screen.js

--- js/screen.js
+++ js/screen.js
@@ -1,3 +1,8 @@
+//graphicalUserInterface
+/*
+* @LUpdate 17/04/2017
+* @VErsion 1.3
+*/
 var graphicalUserInterface = function( setting, _self ){
 		var self = _self || {};
 		var rawMonitor = setting.monitor,
@@ -11,6 +16,7 @@ var graphicalUserInterface = function( setting, _self ){
 			rawMonitor.width  = screen_x;
 			rawMonitor.height = screen_y;
 			
+			
 	["x","y"].map( function( val ){
 			self["screen_"+val ] = val == "x" ? screen_x : screen_y;
  	});
@@ -20,27 +26,36 @@ var graphicalUserInterface = function( setting, _self ){
 	};
 	// reset Monitor
 	self.resetScreen = function( color ){
-		ctxMonitor.fillStyle = color;
-		ctxMonitor.fillRect( 0,0, screen_x, screen_y );
+		var h = 0;
+		while( h <= screen_y ){
+			this.setLine( h, color );
+			h++;
+		}
+		this.refresh( );
+	return this;
 	};
 	// resize Monitor
 	self.resize = function( x, y ){
-		self.screen_x = screen_x = x;
-		self.screen_y = screen_y = y;
+		rawMonitor.width  = self.screen_x = screen_x = x;
+		rawMonitor.height = self.screen_y = screen_y = y;
 		screen_d = ctxMonitor.createImageData( x, y );
 	};
 	["get","set"].map( function( val ){
 		// get & set RawPixel
+		// By offset
 		self[ val+"RawPixel" ] = function( offset, color ){
 			if( val == "set" ){
-				screen_d.data[ offset+0x00 ] = ( color >> 0x10 )&0xff;
-				screen_d.data[ offset+0x01 ] = ( color >> 0x08 )&0xff;
-				screen_d.data[ offset+0x02 ] = ( color )&0xff;
-				screen_d.data[ offset+0x03 ] = 255;
+				
+				screen_d.data[ offset+0x00 ] = ( color >> 0x10 )&0xff;	// R
+				screen_d.data[ offset+0x01 ] = ( color >> 0x08 )&0xff;	// G
+				screen_d.data[ offset+0x02 ] = ( color )&0xff;		   // B
+				screen_d.data[ offset+0x03 ] = setting.rgba ? ( color >> 0x18 )&0xff : 255; // A
+				
 			}else{
 			return ( screen_d.data[ offset+0x00 ] << 0x10 |
 					 screen_d.data[ offset+0x01 ] << 0x08 |
-					 screen_d.data[ offset+0x02 ] );
+					 screen_d.data[ offset+0x02 ] | 
+					( setting.rgba ? screen_d.data[ offset+0x03 ]  : 0 ) << 0x18 );
 			}
 		};
 		// get & set Line
@@ -63,17 +78,22 @@ var graphicalUserInterface = function( setting, _self ){
 		return ( val == "get" ? colors : 1 );
 		};
 		// set & get Pixel
+		// bY point destination x,y
 		self[ val+"Pixel" ] = function( x, y, color ){
 			// ( LineY * screen_x + offset ) * bpp 
 			return self[ val+"RawPixel"].call( self, ( ( ( y * screen_x ) + x )*0x04 ), color );
 		};
 	});
+	
+	self.snapshot = function( ){
+		return rawMonitor.toDataURL( );
+	};
 	// refresh monitor
 	self.refresh = function( ){
 		ctxMonitor.putImageData( screen_d, 0,0 );
 	};
 	// convert int to RRGGBB
-	var intToRgb = self.intToRgb = function( color ){
+	var intToRgb = self.intToRgb = window.intToRgb = function( color ){
 	return { r:(( color >> 0x10 )&0xff),
 			 g:(( color >> 0x08 )&0xff),
 			 b:( color&0xff) 
